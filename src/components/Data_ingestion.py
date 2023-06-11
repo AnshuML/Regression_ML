916aad292400cd9f0775838c4122481ccb7cc069
Anshu826 <anshuucancone@gmail.com>
2023-06-11 18:13:15 +0530

    first commit

---
+++ src/components/Data_ingestion.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionconfig:
+    train_data_path:str=os.path.join('artifcats','train.csv')
+    test_data_path:str=os.path.join('artifcats','test.csv')
+    raw_data_path:str=os.path.join("artifcats",'raw.csv')
+   
+##  create class for data ingestion
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionconfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('data ingestion methods starts')
+        
+        try:
+            df=pd.read_csv("https://raw.githubusercontent.com/krishnaik06/FSDSRegression/main/notebooks/data/gemstone.csv")
+            logging.info('dataset read as pandas DataFrame') 
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info('train test split')
+            train_set,test_set=train_test_split(df,test_size=0.30)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+
+
+        except Exception as e:
+            logging.info('exception occutrd at data ingestion stage')
+            raise CustomException(e,sys)
+        
+
+## run Data Ingestion
+
+if __name__ =='__main__':
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+
+
+
+
+
